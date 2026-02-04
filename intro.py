f95f68053fe9dc2729c906237b91701c95f7d76f
PS Adams <pad@stac.school.nz>
2026-02-05 12:04:25 +1300

    print statement on intro file

---
+++ intro.py
@@ -0,0 +1,8 @@
+''' This program demonstrates print(), data types, variables, inputs and f-strings'''
+
+# print() is a function that outputs whatever is inside the brackets
+# numbers can be included directly in the brackets
+print(123)
+
+# when printing text, it must be in speechmarks which turns it into a string
+print("Hello")
